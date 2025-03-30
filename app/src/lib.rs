db0126c791eaf6159d56e1b86826c09d881caa82
Cory Duplantis <cld251@gmail.com>
2025-03-29 19:18:35 -0500

    Two counters in app

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,13 +1,15 @@
 use shared::AppState;
 
-use egui::{Color32, Painter, Pos2, Rect, Stroke, StrokeKind, Vec2};
-
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn update(ctx: &egui::Context, state: &mut AppState) {
     // Use a central panel and get the painter to draw custom shapes.
     egui::CentralPanel::default().show(ctx, |ui| {
-        if ui.button("Hi!").clicked() {
+        if ui.button("Count by 1").clicked() {
             state.counter += 1;
+        }
+
+        if ui.button("Count by 2").clicked() {
+            state.counter += 2;
         }
 
         ui.label(format!("Counter: {}", state.counter));
